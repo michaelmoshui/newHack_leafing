a7e7c57c556e59c570d999b92fd1d72f220f70af
Chris Kim <sunchaesk@gmail.com>
2022-11-05 12:36:23 -0400

    databaesr

--- newHacks.py
+++ newHacks.py
@@ -1,17 +1,38 @@
-# -*- coding: utf-8 -*-
 """
-Created on Sat Nov  5 07:36:44 2022
-
-@author: micha
+    test a SQLite database connection locally
+    assumes database file is in same location
+    as this .py file
 """
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "Hello World"
+# change to name of your database; add path if necessary
+db_name = "sockmarket.db"
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + db_name
+
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+
+# this variable, db, will be used for all SQLAlchemy commands
+db = SQLAlchemy(app)
+
+# NOTHING BELOW THIS LINE NEEDS TO CHANGE
+# this route will test the database connection and nothing more
+@app.route("/")
+def testdb():
+    try:
+        db.session.query(text("1")).from_statement(text("SELECT 1")).all()
+        return "<h1>It works.</h1>"
+    except Exception as e:
+        # e holds description of the error
+        error_text = "<p>The error:<br>" + str(e) + "</p>"
+        hed = "<h1>Something is broken.</h1>"
+        return hed + error_text
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
